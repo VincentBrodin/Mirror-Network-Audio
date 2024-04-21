cf4098f19dc8debd436bf412514f79d249f8350e
Vincent Brodin <64703732+VincentBrodin@users.noreply.github.com>
2024-04-21 22:10:31 +0200

    Update NetworkAudio.cs

--- NetworkAudio.cs
+++ NetworkAudio.cs
@@ -1,8 +1,7 @@
-﻿using System;
 using Mirror;
 using UnityEngine;
 
-namespace Code.Networking{
+namespace VincentBrodin.Networking{
     [RequireComponent(typeof(AudioSource))]
     public class NetworkAudio : NetworkBehaviour{
         public AudioSource audioSource;
@@ -16,26 +15,88 @@ namespace Code.Networking{
             pitch = audioSource.pitch;
         }
 
+        /// <summary>
+        /// Syncs the Play function from AudioSource to all clients
+        /// </summary>
         public void Play(){
             CmdPlay();
         }
         
         [Command(requiresAuthority = false)]
         private void CmdPlay(){
+            ClientPlay();
+        }
+
+        [ClientRpc]
+        private void ClientPlay(){
             audioSource.pitch = pitch;
             audioSource.volume = volume;
             audioSource.Play();
         }
+
+        /// <summary>
+        /// Syncs the Stop function from AudioSource to all clients
+        /// </summary>
+        public void Stop(){
+            CmdStop();
+        }
+
+        [Command(requiresAuthority = false)]
+        private void CmdStop(){
+            ClientStop();
+        }
+
+        [ClientRpc]
+        private void ClientStop(){
+            audioSource.Stop();
+        }
         
+        /// <summary>
+        /// Syncs the PlayDelay function from AudioSource to all clients
+        /// </summary>
+        /// <param name="delay">The given delay</param>
         public void PlayDelay(float delay){
             CmdPlayDelay(delay);
         }
         
         [Command(requiresAuthority = false)]
         private void CmdPlayDelay(float delay){
+            ClientPlayDelay(delay);
+        }
+        
+        [ClientRpc]
+        private void ClientPlayDelay(float delay){
             audioSource.pitch = pitch;
             audioSource.volume = volume;
             audioSource.PlayDelayed(delay);
+        }
+        
+        /// <summary>
+        /// Syncs the volume between clients
+        /// </summary>
+        /// <param name="newValue">The new volume (0,1)</param>
+        public void SetVolume(float newValue){
+            CmdSetVolume(newValue);
+        }
+
+        [Command(requiresAuthority = false)]
+        private void CmdSetVolume(float newValue){
+            newValue = Mathf.Clamp(newValue, 0, 1);
+            volume = newValue;
+        }
+        
+        /// <summary>
+        /// Syncs the pitch between clients
+        /// </summary>
+        /// <param name="newValue">The new pitch (-3,3)</param>
+        public void SetPitch(float newValue){
+            CmdSetPitch(newValue);
+        }
+
+        [Command(requiresAuthority = false)]
+        private void CmdSetPitch(float newValue){
+            newValue = Mathf.Clamp(newValue, -3, 3);
+            pitch = newValue;
         }
     }
 }
